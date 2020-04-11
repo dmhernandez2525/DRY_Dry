5bec9ade5715f8287e9bc4855fbbb16f849d819e
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-04-11 13:10:08 -0600

    Add button style

--- src/components/DryButton/DryButton.jsx
+++ src/components/DryButton/DryButton.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./DryButton.scss";
 
-const DryButton = ({ text, onClick, className = "standard" }) => {
-  return <div className={className}>{text}</div>;
+const DryButton = ({
+  text = "Button",
+  onClick,
+  className = "btn btn-animated",
+  color = "white"
+}) => {
+  return (
+    <a href="#" className={`${className} btn-${color}`} onClick={onClick}>
+      {text}
+    </a>
+  );
 };
 export default DryButton;
