7f4ca52c1587aa2d0d899e888ad18484c71a65d3
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-04-25 13:41:58 -0600

    Add storybook configre files

---
+++ .storybook/yourTheme.js
@@ -0,0 +1,37 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+
+  // colorPrimary: "hotpink",
+  // colorSecondary: "deepskyblue",
+
+  // UI
+  appBg: "rgba(57, 12, 110, 1)",
+  // appContentBg: "silver",
+  // appBorderColor: "grey",
+  // appBorderRadius: 4,
+
+  // Typography
+  // fontBase: '"Open Sans", sans-serif',
+  // fontCode: "monospace",
+
+  // Text colors
+  textColor: "gold",
+  // textInverseColor: "rgba(255,255,255,0.9)",
+
+  // Toolbar default and active colors
+  barTextColor: "silver",
+  // barSelectedColor: "black",
+  // barBg: "hotpink",
+
+  // Form colors
+  // inputBg: "white",
+  // inputBorder: "silver",
+  // inputTextColor: "black",
+  // inputBorderRadius: 4,
+
+  // brandTitle: "My custom storybook",
+  // brandUrl: "https://example.com",
+  brandImage: "/ComfortOrderLogo.png",
+});
