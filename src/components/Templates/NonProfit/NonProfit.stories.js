9d7768c0da7574a1f77165f4d572afacff852b1d
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-06-04 18:06:41 -0600

    Add faker

--- src/components/Templates/NonProfit/NonProfit.stories.js
+++ src/components/Templates/NonProfit/NonProfit.stories.js
@@ -56,38 +56,38 @@ export const Default = () => {
                 name: "Free",
                 price: 0,
                 details: [
-                  "Curabitur eu eros et risus rutrum elementum.",
-                  "Phasellus a quam lobortis magna viverra tempus.",
-                  "Sed dictum nibh porttitor aliquet pulvinar.",
-                  "Pellentesque vestibulum erat eu ex laoreet volutpat."
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName()
                 ]
               },
               row2: {
                 name: "Plus",
                 price: 9,
                 details: [
-                  "Curabitur eu eros et risus rutrum elementum.",
-                  "Phasellus a quam lobortis magna viverra tempus.",
-                  "Sed dictum nibh porttitor aliquet pulvinar.",
-                  "Pellentesque vestibulum erat eu ex laoreet volutpat.",
-                  "Nam tempor ex auctor nunc gravida, vel facilisis eros sagittis.",
-                  "Nullam quis eros ac eros cursus tempor a quis lorem."
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName()
                 ]
               },
               row3: {
                 name: "Pro",
                 price: 20,
                 details: [
-                  "Curabitur eu eros et risus rutrum elementum.",
-                  "Phasellus a quam lobortis magna viverra tempus.",
-                  "Sed dictum nibh porttitor aliquet pulvinar.",
-                  "Pellentesque vestibulum erat eu ex laoreet volutpat.",
-                  "Nam tempor ex auctor nunc gravida, vel facilisis eros sagittis.",
-                  "Nullam quis eros ac eros cursus tempor a quis lorem.",
-                  "Proin vel nisi sed mauris tristique tincidunt vulputate sit amet nulla.",
-                  "Phasellus vulputate nisi in libero euismod, lacinia mattis ligula cursus.",
-                  "Donec porta enim vitae mollis bibendum.",
-                  "Fusce in erat id diam porttitor pellentesque et non tortor."
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName(),
+                  faker.commerce.productName()
                 ]
               }
             }
