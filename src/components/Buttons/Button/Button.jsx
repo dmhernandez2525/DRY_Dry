a049a489b03a0716e5d26d854bcbfd6d9f47aa85
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-06-19 15:10:14 -0600

    Cleanup button

--- src/components/Buttons/Button/Button.jsx
+++ src/components/Buttons/Button/Button.jsx
@@ -5,35 +5,25 @@ import "./Button.scss";
 const Button = ({
   id,
   name,
-  userTip,
   onClick,
   onChange,
   onBlur,
   onFocus,
   disable,
   className,
-  errorMes,
-  styles,
   passProps
 }) => {
-  return (
-    <div onClick={() => onClick("DryButton")} className="dry-button">
-      DryButton
-    </div>
-  );
+  return <div className="dry-button">DryButton</div>;
 };
 
 Button.defaultProps = {
   id: "",
   name: "",
-  userTip: "",
   onClick: null,
   onChange: null,
   onBlur: null,
   disable: false,
   className: "",
-  errorMes: "",
-  styles: null,
   passProps: null
 };
 
