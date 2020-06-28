183714193b057f0fa3794067bc60b3b38a070e35
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-06-28 02:52:24 -0600

    Clean up storys

--- src/components/FormsHelpers/CustomScrollbar/CustomScrollbar.stories.js
+++ src/components/FormsHelpers/CustomScrollbar/CustomScrollbar.stories.js
@@ -13,7 +13,7 @@ import {
   date,
   select,
   files,
-  button,
+  button
 } from "@storybook/addon-knobs/react";
 
 import CustomScrollbar from "./CustomScrollbar";
@@ -23,7 +23,7 @@ import README from "./README.md";
 export const actionsData = {
   onClick: action("onClick"),
   onChange: action("onChange"),
-  onBlur: action("onBlur"),
+  onBlur: action("onBlur")
 };
 
 export const Default = () => {
@@ -41,52 +41,6 @@ export const Default = () => {
     />
   );
 };
-
-export const Disable = () => (
-  <CustomScrollbar
-    id={text("id", "")}
-    name={text("name", "")}
-    userTip={text("userTip", "")}
-    disable={boolean("disable", true)}
-    className={text("className", "")}
-    errorMes={text("errorMes", "")}
-    styles={object("styles", {})}
-    passProps={object("passProps", {})}
-    {...actionsData}
-  />
-);
-
-export const Error = () => (
-  <CustomScrollbar
-    id={text("id", "")}
-    name={text("name", "")}
-    userTip={text("userTip", "")}
-    disable={boolean("disable", false)}
-    className={text("className", "")}
-    errorMes={text("errorMes", "DemoError")}
-    styles={object("styles", {})}
-    passProps={object("passProps", {})}
-    {...actionsData}
-  />
-);
-
-const userTip =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo arcu sit amet malesuada tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus finibus eget ex id consectetur. Curabitur scelerisque, purus sit amet auctor rhoncus, lorem sem blandit ipsum, vitae convallis quam magna nec leo. Cras aliquet metus quis tortor pharetra hendrerit. Sed placerat interdum hendrerit. Mauris convallis lacus enim, a dignissim dolor congue at. Mauris eget consequat nisl. Donec lobortis ligula eu velit pharetra, sit amet bibendum sapien molestie. Donec vestibulum blandit massa eget commodo. Aliquam aliquet tristique tortor, ac facilisis sem rhoncus id. Sed bibendum diam nisl, in aliquam risus venenatis quis. Praesent ipsum metus, dictum nec aliquam id, venenatis et ante. Quisque sit amet suscipit elit. Pellentesque eget turpis a ex ullamcorper hendrerit id eget dolor.";
-
-export const UserTip = () => (
-  <CustomScrollbar
-    id={text("id", "")}
-    name={text("name", "")}
-    userTip={text("userTip", userTip)}
-    disable={boolean("disable", false)}
-    className={text("className", "")}
-    errorMes={text("errorMes", "")}
-    styles={object("styles", {})}
-    passProps={object("passProps", {})}
-    {...actionsData}
-  />
-);
-
 storiesOf("Components/FormsHelpers/CustomScrollbar", module)
   .addParameters({
     zeplinLink: "",
@@ -95,10 +49,7 @@ storiesOf("Components/FormsHelpers/CustomScrollbar", module)
       sidebar: [README, CustomScrollbarPropTable],
       StoryPreview: ({ children }) => {
         return <div>{children}</div>;
-      },
-    },
+      }
+    }
   })
-  .add("Default", () => <Default />)
-  .add("Error", () => <Error />)
-  .add("UserTip", () => <UserTip />)
-  .add("Disable", () => <Disable />);
+  .add("Default", () => <Default />);
