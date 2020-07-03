2681f65d937b8ca9d89360d1287c6e4fa62f2d90
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-07-03 17:40:53 -0600

    Move Dry Docs

--- src/@DryDocs/DryDocs.stories.js
+++ src/@DryDocs/DryDocs.stories.js
@@ -3,24 +3,24 @@ import { storiesOf } from "@storybook/react";
 
 // quick-start
 import DryDocs from "./DryDocs";
-import QuickReadme from "../../docs/quick-start/README.md";
-import QuickComponents from "../../docs/quick-start/components.md";
+import QuickReadme from "../docs/quick-start/README.md";
+import QuickComponents from "../docs/quick-start/components.md";
 
 // Contributing
-import Creating from "../../docs/contributing/creating.md";
-import Documentation from "../../docs/contributing/documentation.md";
-import Release from "../../docs/contributing/release.md";
-import Review from "../../docs/contributing/review.md";
-import Setup from "../../docs/contributing/setup.md";
-import Storybook from "../../docs/contributing/storybook.md";
-import Styling from "../../docs/contributing/styling.md";
-import Testing from "../../docs/contributing/testing.md";
+import Creating from "../docs/contributing/creating.md";
+import Documentation from "../docs/contributing/documentation.md";
+import Release from "../docs/contributing/release.md";
+import Review from "../docs/contributing/review.md";
+import Setup from "../docs/contributing/setup.md";
+import Storybook from "../docs/contributing/storybook.md";
+import Styling from "../docs/contributing/styling.md";
+import Testing from "../docs/contributing/testing.md";
 
 // Testing
-import TestingS from "../../docs/testing/local-integration-testing.md";
+import TestingS from "../docs/testing/local-integration-testing.md";
 
 // Issues
-import Issues from "../../docs/issues.md";
+import Issues from "../docs/issues.md";
 
 storiesOf("Docs/QuickStart", module)
   .addParameters({
