ff7fb15cc40fa02298bcd847fd715a7ca4ed107b
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-07-05 14:40:00 -0600

    Add sticker sheet

--- src/components/Features/Coupon/Coupon.stories.js
+++ src/components/Features/Coupon/Coupon.stories.js
@@ -1,63 +1,58 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import {
+  withKnobs,
+  knob,
+  text,
+  boolean,
+  number,
+  color,
+  object,
+  array,
+  date,
+  select,
+  files,
+  button
+} from "@storybook/addon-knobs/react";
 
-        import React from "react";
-        import { storiesOf } from "@storybook/react";
-        import { action } from "@storybook/addon-actions";
-        import { 
-            withKnobs, 
-            knob,
-            text,
-            boolean,
-            number,
-            color,
-            object,
-            array,
-            date,
-            select,
-            files,
-            button } from "@storybook/addon-knobs/react";
-        
-        import Coupon from "./Coupon";
-        import CouponPropTable from "./CouponPropTable.md";
-        import README from "./README.md";
-        
-        export const actionsData = {
-            onClick: action("onClick"),
-            onChange: action("onChange"),
-            onBlur: action("onBlur")
-        };
-        
-        export const Default = () => {
-            return <Coupon             
-                id={text("id","")}
-                name={text("name","")}
-                userTip={text("userTip","")}
-                disable={boolean("disable",false)}
-                className={text("className","")}
-                errorMes={text("errorMes","")}
-                styles={object("styles", {})}
-                passProps={object("passProps", {})} 
-                {...actionsData} 
-            />;
-        };
-        
-        
+import Coupon from "./Coupon";
+import CouponPropTable from "./CouponPropTable.md";
+import README from "./README.md";
 
-        storiesOf("Features/Coupon", module)
-        .addParameters({
-          zeplinLink:
-            "",
-          readme: {
-            codeTheme: "atom-dark",
-            sidebar: [README, CouponPropTable],
-            StoryPreview: ({ children }) => {
-              return (
-                <div>
-                  {children}
-                </div>
-              );
-            }
-          }
-        })
-        .add("Default", () => <Default />)
-      
-        
+export const actionsData = {
+  onClick: action("onClick"),
+  onChange: action("onChange"),
+  onBlur: action("onBlur")
+};
+
+export const Default = () => {
+  return (
+    <Coupon
+      id={text("id", "")}
+      name={text("name", "")}
+      userTip={text("userTip", "")}
+      disable={boolean("disable", false)}
+      className={text("className", "")}
+      errorMes={text("errorMes", "")}
+      styles={object("styles", {})}
+      passProps={object("passProps", {})}
+      {...actionsData}
+    />
+  );
+};
+
+storiesOf("Features/Coupon", module)
+  .addParameters({
+    zeplinLink: "",
+    readme: {
+      codeTheme: "atom-dark",
+      sidebar: [README, CouponPropTable],
+      StoryPreview: ({ children }) => {
+        return <div>{children}</div>;
+      }
+    }
+  })
+  .add("Default", () => <Default />);
+
+storiesOf("Docs/Sticker Sheet", module).add("Coupon", () => <Default />);
